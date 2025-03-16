9c5744eb3dc532c2a6f7387b5179525cd3f51c42
Aryan Shukla <as3061693@gmail.com>
2025-03-16 09:47:43 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
           <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
             <div className="order-2 md:order-1">
               <h1 className="text-4xl md:text-6xl font-bold mb-4">
-                Hi, I am <br />
+                Hi! I am <br />
                 <span className="neon-text-green">Aryan Shukla</span>
               </h1>
               <p className="text-lg md:text-xl text-gray-300 mb-8 leading-relaxed">
